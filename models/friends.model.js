914bec651898801815f36e79b60e8abe895eb96b
José Sampaio <jose@pippa.com.br>
2022-06-28 17:03:14 -0300

    setup

---
+++ models/friends.model.js
@@ -0,0 +1,16 @@
+const friends = [
+  {
+    id: 0,
+    name: "Sir Albert Einstein",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Sir Nikola Teslaa",
+  },
+];
+
+module.exports = friends;
