790fbe347b76ce116576986979da872661f3576c
José Sampaio <jose@pippa.com.br>
2022-07-05 11:17:15 -0300

    Separation of concerns

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,13 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", 'images', "skimountain.jpg"));
+  //res.sendFile(path.join(__dirname, "..", "public", 'images', "skimountain.jpg"));
   // res.send("<ul><li>Hello Albert!</li></ul>");
+
+  res.render('messages', {
+    title: 'Messages to my friends!',
+    friend:'Elon Musk'
+  })
 }
 
 function postMessage(req, res) {
