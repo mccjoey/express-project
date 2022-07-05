5622c9fac46f48634b538a65506a8c74bf9aae25
José Sampaio <jose@pippa.com.br>
2022-07-05 11:09:47 -0300

    Templating engines

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
 
 const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -15,6 +19,15 @@ app.use((req, res, next) => {
 
 app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "My friends are very clever",
+    caption:"Let's go skiing!"
+  })
+
+})
+
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
 
